bac230b5892c5cd7b74514d028e857c2d4df095d
JianZG <2992265870@qq.com>
2017-10-12 16:49:07 +0800

    -

--- config/index.js
+++ config/index.js
@@ -7,7 +7,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'http://cdn.jiguo.com/special/xiapu/mb/',
+    // assetsPublicPath: 'http://cdn.jiguo.com/special/xiapu/mb/',
+    assetsPublicPath: 'https://jiangzhenggeng.github.io/xiapu/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
